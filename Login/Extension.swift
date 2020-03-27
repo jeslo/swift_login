163eb12afac1081ef528d7611f738d6ea43f3979
jesloam <jesloa@gmail.com>
2020-03-27 10:49:36 +0530

    validation on going

---
+++ Login/Extension.swift
@@ -0,0 +1,30 @@
+//
+//  Extension.swift
+//  Login
+//
+//  Created by Joe on 26/03/20.
+//  Copyright © 2020 jess. All rights reserved.
+//
+
+import UIKit
+
+class Extension: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
